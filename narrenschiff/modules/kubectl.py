c093323d0a4e96524f311dd7dd952ff716f3a980
Petar Nikolovski <petarGitNik@users.noreply.github.com>
2019-08-20 17:04:52 +0200

    Sanitize filenames (expand filepath) before executing kubectl

--- narrenschiff/modules/kubectl.py
+++ narrenschiff/modules/kubectl.py
@@ -1,4 +1,6 @@
+import os
 from contextlib import suppress
+
 from narrenschiff.modules.common import NarrenschiffModule
 
 
@@ -11,11 +13,12 @@ class Kubectl(NarrenschiffModule):
     def execute(self):
         command = self.command.get('command')
 
+        self.sanitize_filenames()
         self.update_filename_argument()
         args = self.command.get('args')
         flags = []
         for key in args:
-            flags.append('--{} "{}"'.format(key, args[key]))
+            flags.append("--{} '{}'".format(key, args[key]))
 
         cmd = ' '.join([Kubectl.kubectl, command, *flags])
         print(cmd)
@@ -31,3 +34,24 @@ class Kubectl(NarrenschiffModule):
             filenames = self.command['args']['filename']
             if isinstance(filenames, list):
                 self.command['args']['filename'] = ','.join(filenames)
+
+    def sanitize_filenames(self):
+        """
+        Change relative paths to absolute paths corresponding to rendered
+        files.
+
+        :return: Void
+        :rtype: ``None``
+        """
+        try:
+            filename = self.command['args']['filename']
+            path = os.path.join(self.template.tmp, filename)
+            self.command['args']['filename'] = path
+        except TypeError:
+            filenames = self.command['args']['filename']
+            paths = []
+            for filename in filenames:
+                paths.append(os.path.join(self.template.tmp, filename))
+            self.command['args']['filename'] = paths
+        except KeyError:
+            pass

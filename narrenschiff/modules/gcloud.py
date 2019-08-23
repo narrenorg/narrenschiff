8a77742dfb0a401c471e309df7f625fbaf804ca3
Petar Nikolovski <petarGitNik@users.noreply.github.com>
2019-08-23 13:05:45 +0200

    Finalize first version of gcloud module

--- narrenschiff/modules/gcloud.py
+++ narrenschiff/modules/gcloud.py
@@ -1,15 +1,34 @@
-from narrenschiff.modules.common import NarrenschiffModule
 import subprocess
+
+from narrenschiff.modules.common import NarrenschiffModule
 
 
 class Gcloud(NarrenschiffModule):
-    """``GCloud`` module."""
+    """``gcloud`` module."""
 
     gcloud_cmd = 'gcloud'
 
     def execute(self):
         command = self.command.get('command')
+        args = self.command.get('args', {})
 
-        cmd = ' '.join([Gcloud.gcloud_cmd, command])
-        print(cmd)
-        # subprocess.check_call(cmd, shell=True)
+        flags = []
+        for flag, value in args.items():
+            flags.append("--{} '{}'".format(flag, args[flag]))
+
+        cmd = ' '.join([Gcloud.gcloud_cmd, command, *flags])
+
+        # When you enable some API the output will be empty
+        # It's OK to pipe to stdout instead of catching with check in the
+        # tasks engine - though it would create problems with commands that
+        # require user input
+        subprocess.run(
+            cmd,
+            shell=True,
+            check=True,
+            # stdout=subprocess.PIPE,
+            # stderr=subprocess.PIPE
+        )
+        # output = process.stdout if process.stdout else process.stderr
+        # color = 'green' if process.stdout else 'red'
+        # click.secho(output.decode('utf-8'), fg=color)

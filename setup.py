d8f5bea1f4546b269040b816ed667fde81a7f3d2
Petar Nikolovski <petarGitNik@users.noreply.github.com>
2019-08-26 16:08:11 +0200

    Add gitlab-ci configuration file

--- setup.py
+++ setup.py
@@ -1,9 +1,20 @@
+import os
 from setuptools import setup
+
+
+with open(os.path.join(os.path.dirname(__file__), 'README.md'), 'r') as readme:
+    README = readme.read()
+
 
 setup(
     name='narrenschiff',
-    version='0.0.1',
-    py_modules=['narrenschiff'],
+    version=os.getenv('CI_COMMIT_TAG'),
+    packages=['narrenschiff'],
+    description='k8s deployment and configuration management tool',
+    long_description=README,
+    url='https://example.com',
+    maintainer='Petar Nikolovski',
+    maintainer_email='nikolovski@brainshuttle.com',
     install_requires=[
         'click', 'PyYAML', 'Jinja2', 'cryptography'
     ],
